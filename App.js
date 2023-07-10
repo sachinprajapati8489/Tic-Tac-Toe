1fc97d289ada3deefee4e9f0130508ab580459fe
sachinparjapati011 <sachinparjapati011@gmail.com>
2023-07-10 11:29:20 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Tictactoe from './Tictactoe';
+
+function App(){
+  return(
+    <div className='app'>
+      <Tictactoe/>
+    </div>
+  )
+}
+
+export default App;
